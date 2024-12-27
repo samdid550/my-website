260862e24285aed17b03223ce3b91543809c3a17
Samer <samdid550@gmail.com>
2024-12-26 21:20:04 -0500

    Initial commit4

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function showAlert() {
 
 window.addEventListener('load', function() {
     setTimeout(function() {
-        document.getElementById('graduated-image').style.opacity = 1;
+        document.getElementById('graduated-image').style.opacity = 1;  // Show the image after 1 second
     }, 1000); // Delay of 1 second before showing the image
 });
 
@@ -13,6 +13,7 @@ window.addEventListener('scroll', function() {
     const imagePosition = image.getBoundingClientRect().top;
     const screenPosition = window.innerHeight;
 
+    // Show the image when it comes into the viewport (scroll)
     if (imagePosition < screenPosition) {
         image.style.opacity = 1;
     }
